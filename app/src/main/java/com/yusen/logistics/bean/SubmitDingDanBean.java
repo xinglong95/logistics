83bffe5e12be3ea6b6ea18174611ad523998a7b9
xinglong95 <844584171@qq.com>
2018-09-14 20:42:26 +0800

    1、上传商品相关接口绑定

---
+++ app/src/main/java/com/yusen/logistics/bean/SubmitDingDanBean.java
@@ -0,0 +1,436 @@
+package com.yusen.logistics.bean;
+
+import java.util.List;
+
+public class SubmitDingDanBean {
+
+    /**
+     * W_ID : 0
+     * W_DeliveryDate : 2018-09-13
+     * W_F_Province : 北海道
+     * W_F_City : 札幌市
+     * W_F_District : 白石区
+     * W_F_Address : 测试地址
+     * W_F_Name : 陈龙军
+     * W_F_Tel : 18562873018
+     * W_S_Province : 山东省
+     * W_S_City : 青岛市
+     * W_S_District : 市北区
+     * W_S_Address : 台东邮电局
+     * W_S_Name : 吴鹏
+     * W_S_Tel : 18562873018
+     * W_D_Name :
+     * W_D_Tel :
+     * IdCard_Name : 吴鹏
+     * IdCard_Number : 370203198802203767
+     * IdCard_Sex : 男
+     * IdCard_National : 汉族
+     * IdCard_BrithDate : 1988-02-20
+     * IdCard_Address : 测试身份证地址
+     * IdCard_Organ : 青岛市北公安局
+     * IdCard_BeginDate : 2011
+     * IdCard_EndDate : 2028
+     * IdCard_Head : /Image/IdCard/1.png
+     * IdCard_Tail : /Image/IdCard/2.png
+     */
+
+    private String W_ID;
+    private String W_DeliveryDate;
+    private String W_F_Province;
+    private String W_F_City;
+    private String W_F_District;
+    private String W_F_Address;
+    private String W_F_Name;
+    private String W_F_Tel;
+    private String W_S_Province;
+    private String W_S_City;
+    private String W_S_District;
+    private String W_S_Address;
+    private String W_S_Name;
+    private String W_S_Tel;
+    private String W_D_Name;
+    private String W_D_Tel;
+    private String IdCard_Name;
+    private String IdCard_Number;
+    private String IdCard_Sex;
+    private String IdCard_National;
+    private String IdCard_BrithDate;
+    private String IdCard_Address;
+    private String IdCard_Organ;
+    private String IdCard_BeginDate;
+    private String IdCard_EndDate;
+    private String IdCard_Head;
+    private String IdCard_Tail;
+    private List<ShangPinItem> Sub;
+
+    public List<ShangPinItem> getSub() {
+        return Sub;
+    }
+
+    public void setSub(List<ShangPinItem> sub) {
+        Sub = sub;
+    }
+
+    public String getW_ID() {
+        return W_ID;
+    }
+
+    public void setW_ID(String W_ID) {
+        this.W_ID = W_ID;
+    }
+
+    public String getW_DeliveryDate() {
+        return W_DeliveryDate;
+    }
+
+    public void setW_DeliveryDate(String W_DeliveryDate) {
+        this.W_DeliveryDate = W_DeliveryDate;
+    }
+
+    public String getW_F_Province() {
+        return W_F_Province;
+    }
+
+    public void setW_F_Province(String W_F_Province) {
+        this.W_F_Province = W_F_Province;
+    }
+
+    public String getW_F_City() {
+        return W_F_City;
+    }
+
+    public void setW_F_City(String W_F_City) {
+        this.W_F_City = W_F_City;
+    }
+
+    public String getW_F_District() {
+        return W_F_District;
+    }
+
+    public void setW_F_District(String W_F_District) {
+        this.W_F_District = W_F_District;
+    }
+
+    public String getW_F_Address() {
+        return W_F_Address;
+    }
+
+    public void setW_F_Address(String W_F_Address) {
+        this.W_F_Address = W_F_Address;
+    }
+
+    public String getW_F_Name() {
+        return W_F_Name;
+    }
+
+    public void setW_F_Name(String W_F_Name) {
+        this.W_F_Name = W_F_Name;
+    }
+
+    public String getW_F_Tel() {
+        return W_F_Tel;
+    }
+
+    public void setW_F_Tel(String W_F_Tel) {
+        this.W_F_Tel = W_F_Tel;
+    }
+
+    public String getW_S_Province() {
+        return W_S_Province;
+    }
+
+    public void setW_S_Province(String W_S_Province) {
+        this.W_S_Province = W_S_Province;
+    }
+
+    public String getW_S_City() {
+        return W_S_City;
+    }
+
+    public void setW_S_City(String W_S_City) {
+        this.W_S_City = W_S_City;
+    }
+
+    public String getW_S_District() {
+        return W_S_District;
+    }
+
+    public void setW_S_District(String W_S_District) {
+        this.W_S_District = W_S_District;
+    }
+
+    public String getW_S_Address() {
+        return W_S_Address;
+    }
+
+    public void setW_S_Address(String W_S_Address) {
+        this.W_S_Address = W_S_Address;
+    }
+
+    public String getW_S_Name() {
+        return W_S_Name;
+    }
+
+    public void setW_S_Name(String W_S_Name) {
+        this.W_S_Name = W_S_Name;
+    }
+
+    public String getW_S_Tel() {
+        return W_S_Tel;
+    }
+
+    public void setW_S_Tel(String W_S_Tel) {
+        this.W_S_Tel = W_S_Tel;
+    }
+
+    public String getW_D_Name() {
+        return W_D_Name;
+    }
+
+    public void setW_D_Name(String W_D_Name) {
+        this.W_D_Name = W_D_Name;
+    }
+
+    public String getW_D_Tel() {
+        return W_D_Tel;
+    }
+
+    public void setW_D_Tel(String W_D_Tel) {
+        this.W_D_Tel = W_D_Tel;
+    }
+
+    public String getIdCard_Name() {
+        return IdCard_Name;
+    }
+
+    public void setIdCard_Name(String IdCard_Name) {
+        this.IdCard_Name = IdCard_Name;
+    }
+
+    public String getIdCard_Number() {
+        return IdCard_Number;
+    }
+
+    public void setIdCard_Number(String IdCard_Number) {
+        this.IdCard_Number = IdCard_Number;
+    }
+
+    public String getIdCard_Sex() {
+        return IdCard_Sex;
+    }
+
+    public void setIdCard_Sex(String IdCard_Sex) {
+        this.IdCard_Sex = IdCard_Sex;
+    }
+
+    public String getIdCard_National() {
+        return IdCard_National;
+    }
+
+    public void setIdCard_National(String IdCard_National) {
+        this.IdCard_National = IdCard_National;
+    }
+
+    public String getIdCard_BrithDate() {
+        return IdCard_BrithDate;
+    }
+
+    public void setIdCard_BrithDate(String IdCard_BrithDate) {
+        this.IdCard_BrithDate = IdCard_BrithDate;
+    }
+
+    public String getIdCard_Address() {
+        return IdCard_Address;
+    }
+
+    public void setIdCard_Address(String IdCard_Address) {
+        this.IdCard_Address = IdCard_Address;
+    }
+
+    public String getIdCard_Organ() {
+        return IdCard_Organ;
+    }
+
+    public void setIdCard_Organ(String IdCard_Organ) {
+        this.IdCard_Organ = IdCard_Organ;
+    }
+
+    public String getIdCard_BeginDate() {
+        return IdCard_BeginDate;
+    }
+
+    public void setIdCard_BeginDate(String IdCard_BeginDate) {
+        this.IdCard_BeginDate = IdCard_BeginDate;
+    }
+
+    public String getIdCard_EndDate() {
+        return IdCard_EndDate;
+    }
+
+    public void setIdCard_EndDate(String IdCard_EndDate) {
+        this.IdCard_EndDate = IdCard_EndDate;
+    }
+
+    public String getIdCard_Head() {
+        return IdCard_Head;
+    }
+
+    public void setIdCard_Head(String IdCard_Head) {
+        this.IdCard_Head = IdCard_Head;
+    }
+
+    public String getIdCard_Tail() {
+        return IdCard_Tail;
+    }
+
+    public void setIdCard_Tail(String IdCard_Tail) {
+        this.IdCard_Tail = IdCard_Tail;
+    }
+
+    public class ShangPinItem{
+
+        /**
+         * WS_ID : 0
+         * P_ID : 2
+         * P_Brand : SK2
+         * P_BrandID : 5
+         * P_Number : 6923410717242
+         * P_Name : 随身数码音乐音响
+         * P_Spec : 规格不大
+         * P_Weight : 100.00
+         * P_Price : 150.00
+         * P_Volume : 120.00
+         * P_Type : 药妆
+         * P_TypeID : 1
+         * WS_FirstCount : 2.00
+         * WS_Money : 300.00
+         */
+
+        private String WS_ID;
+        private String P_ID;
+        private String P_Brand;
+        private String P_BrandID;
+        private String P_Number;
+        private String P_Name;
+        private String P_Spec;
+        private String P_Weight;
+        private String P_Price;
+        private String P_Volume;
+        private String P_Type;
+        private String P_TypeID;
+        private String WS_FirstCount;
+        private String WS_Money;
+
+        public String getWS_ID() {
+            return WS_ID;
+        }
+
+        public void setWS_ID(String WS_ID) {
+            this.WS_ID = WS_ID;
+        }
+
+        public String getP_ID() {
+            return P_ID;
+        }
+
+        public void setP_ID(String P_ID) {
+            this.P_ID = P_ID;
+        }
+
+        public String getP_Brand() {
+            return P_Brand;
+        }
+
+        public void setP_Brand(String P_Brand) {
+            this.P_Brand = P_Brand;
+        }
+
+        public String getP_BrandID() {
+            return P_BrandID;
+        }
+
+        public void setP_BrandID(String P_BrandID) {
+            this.P_BrandID = P_BrandID;
+        }
+
+        public String getP_Number() {
+            return P_Number;
+        }
+
+        public void setP_Number(String P_Number) {
+            this.P_Number = P_Number;
+        }
+
+        public String getP_Name() {
+            return P_Name;
+        }
+
+        public void setP_Name(String P_Name) {
+            this.P_Name = P_Name;
+        }
+
+        public String getP_Spec() {
+            return P_Spec;
+        }
+
+        public void setP_Spec(String P_Spec) {
+            this.P_Spec = P_Spec;
+        }
+
+        public String getP_Weight() {
+            return P_Weight;
+        }
+
+        public void setP_Weight(String P_Weight) {
+            this.P_Weight = P_Weight;
+        }
+
+        public String getP_Price() {
+            return P_Price;
+        }
+
+        public void setP_Price(String P_Price) {
+            this.P_Price = P_Price;
+        }
+
+        public String getP_Volume() {
+            return P_Volume;
+        }
+
+        public void setP_Volume(String P_Volume) {
+            this.P_Volume = P_Volume;
+        }
+
+        public String getP_Type() {
+            return P_Type;
+        }
+
+        public void setP_Type(String P_Type) {
+            this.P_Type = P_Type;
+        }
+
+        public String getP_TypeID() {
+            return P_TypeID;
+        }
+
+        public void setP_TypeID(String P_TypeID) {
+            this.P_TypeID = P_TypeID;
+        }
+
+        public String getWS_FirstCount() {
+            return WS_FirstCount;
+        }
+
+        public void setWS_FirstCount(String WS_FirstCount) {
+            this.WS_FirstCount = WS_FirstCount;
+        }
+
+        public String getWS_Money() {
+            return WS_Money;
+        }
+
+        public void setWS_Money(String WS_Money) {
+            this.WS_Money = WS_Money;
+        }
+    }
+}
